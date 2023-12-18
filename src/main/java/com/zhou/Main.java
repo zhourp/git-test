e61608c858d0f5cc0d49a567d14351bf8bec431c
zhourp <48310553+zhourp@users.noreply.github.com>
2023-12-18 17:38:19 +0800

    pull

--- src/main/java/com/zhou/Main.java
+++ src/main/java/com/zhou/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
-    }
+        System.out.println("pull test");
+    } 
 }
