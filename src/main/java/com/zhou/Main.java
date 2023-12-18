de6cc3e35052c450391d517733ffc696b5ccff73
zhourp <48310553+zhourp@users.noreply.github.com>
2023-12-18 16:44:00 +0800

    第一次提交

---
+++ src/main/java/com/zhou/Main.java
@@ -0,0 +1,9 @@
+package com.zhou;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("这是第一个git管理的项目");
+    }
+}
